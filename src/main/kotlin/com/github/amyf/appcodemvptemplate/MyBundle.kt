73cf925b1144c2b83819e4164aa582db27d00b70
GitHub Action <action@github.com>
2022-08-22 10:38:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/amyf/appcodemvptemplate/MyBundle.kt
+++ src/main/kotlin/com/github/amyf/appcodemvptemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.amyf.appcodemvptemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/amyf/appcodemvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/amyf/appcodemvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.amyf.appcodemvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.amyf.appcodemvptemplate.MyBundle
 
 class MyApplicationService {
 
